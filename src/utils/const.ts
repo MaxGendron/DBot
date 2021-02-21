fb371c92972ce25871a5d484ac311d6271b63263
Maxime <gendron.maxime1@gmail.com>
2021-02-20 22:28:56 -0500

    Added paging to inventory command

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,6 @@
 export class Const {
   public static readonly EmbedColor = '#bd3c00';
   public static readonly AllType = 'all';
+  public static readonly DefaultPagingIndex = 0;
+  public static readonly DefaultPagingRange = 10;
 }
